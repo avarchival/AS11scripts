af36157950e70ee64e25acccef422b0d3b86a2da
Eoin O'Donohoe <7ampersand7@gmail.com>
2016-09-09 23:05:14 +0100

    Update conformance.py

--- conformance.py
+++ conformance.py
@@ -14,7 +14,7 @@ starting_dir = sys.argv[1]
 for dirpath, dirnames, filenames in os.walk(starting_dir):
     for filename in [f for f in filenames if f.endswith(".mxf")]:
         full_path = os.path.join(dirpath, filename)
-        mediainfo_xml = subprocess.check_output(['MediaInfo', '--Output=XML', '-f', full_path])
+        mediainfo_xml = subprocess.check_output(['mediainfo', '--Output=XML', '-f', full_path])
         
         file_no_path = os.path.basename(full_path)
         file_no_extension = os.path.splitext(os.path.basename(file_no_path))[0]
